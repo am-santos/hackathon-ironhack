8f4d64c224452733fab5eaa69155a68bfcabb93d
am-santos <antopuppe@gmail.com>
2020-05-29 14:02:28 +0100

    structure

--- middleware/bind-user-to-view-locals.js
+++ middleware/bind-user-to-view-locals.js
@@ -4,6 +4,8 @@ module.exports = (req, res, next) => {
   res.locals.user = req.user;
 
   if (req.user) {
+    console.log('req.user.role', req.user.role);
+    console.log('res.locals.user.isOwner', res.locals.user.isOwner);
     switch (req.user.role) {
       case 'client':
         res.locals.user.isClient = true;
