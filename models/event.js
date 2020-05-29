832c69354631dca06ab44e2a54ee293aa6168da9
cristianekoshino <cristianekoshino@gmail.com>
2020-05-29 12:24:28 +0100

    added event model

---
+++ models/event.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: {
+        type: String,
+        default: 'Point'
+      },
+      coordinates: [
+        {
+          type: Number,
+          min: -180,
+          max: 180
+        }
+      ]
+    },
+    address: {
+      type: String
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    endDate: {
+      type: Date,
+      required: true
+    },
+    photo: {
+      type: String
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    timestamps: {
+      createdAt: 'createdDate',
+      updatedAt: 'updatedDate'
+    }
+  }
+);
+
+eventSchema.index({ location: '2dsphere' });
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
